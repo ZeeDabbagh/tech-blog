b063fbe0787f1640651e2eba53f94201d6bc2983
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-06-28 16:58:27 -0400

    separated blog get routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,34 +2,6 @@ const router = require('express').Router()
 const { Blog, Comment, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
-
-
-//Get one blog
-
-router.get('/:blog_id', withAuth, async (req, res) => {
-
-    try{
-        const blogData = await Blog.findByPk( req.params.blog_id, {
-          include:[
-            {model: User, attributes:'name'},
-          {
-            model: Comment,
-            attributes: ['comment_text', 'comment_date'],
-            include: {model: User, attributes: ['name']}
-          }],
-        });
-
-        const blog = blogData.get({plain: true})
-        const canEdit = req.session.user_id === blog.user_id
-        res.render('blog', {blog, logged_in: req.session.logged_in, canEdit})
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    } 
-  })
-
-
 //Post new blog
 router.post('/', withAuth, async (req, res) => {
     
